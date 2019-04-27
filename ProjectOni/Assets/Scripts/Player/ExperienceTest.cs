0558877d1299a98ffdfe0283165daeb9fffb1625
Unknown <lucho.elp40@gmail.com>
2019-04-27 11:35:57 -0300

    Player systems
    -life system added
    -experience system added

---
+++ ProjectOni/Assets/Scripts/Player/ExperienceTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class ExperienceTest : MonoBehaviour
+{
+    private PlayerExperience pExp;
+
+    private void Start() {
+        pExp = gameObject.GetComponent<PlayerExperience>();
+    }
+    private void Update() {
+        if(Input.GetButton("Fire3")){
+            pExp.Experience = 10;
+        }
+    }
+}
---
+++ ProjectOni/Assets/Scripts/Player/PlayerLife.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerLife : MonoBehaviour
+{
+    [SerializeField]private float life = 100;
+
+    public float Life{
+        get{return life;}
+        set{
+            if(life-value <= 0){
+                //death resolve
+            }else{
+                life -= value;
+            }
+        }
+    }
+}
