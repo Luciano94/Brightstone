08bc8117716f6e025b9b972deecba38123aebd6d
Unknown <lucho.elp40@gmail.com>
2019-04-28 00:56:40 -0300

    Camera Movement
    -Render of the camera now show the active room
    -minimap camera now is more small

--- ProjectOni/Assets/Scripts/Camera/CameraFollow.cs
+++ ProjectOni/Assets/Scripts/Camera/CameraFollow.cs
@@ -3,16 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour{
-    [SerializeField]private GameObject player;
 
-    private void Start() {
-        player.transform.position = new Vector3(transform.position.x, transform.position.y,
-                                                player.transform.position.z);
+    [SerializeField]Camera miniMap;
+    private float speed = 3; 
+	private Vector3 initPos;
+	private Vector3 targetPos;
+	private float accum;
+    private bool needMove = false;
+
+    public void MoveTo(Vector3 pos){
+        targetPos = new Vector3 (pos.x, pos.y , transform.position.z);
+        initPos = transform.position;
+        accum = 0.0f;
+        needMove = true;
     }
 
-    // Update is called once per frame
-    void Update(){
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y,
-                                        transform.position.z);;
-    }
+	void FixedUpdate () {
+        if(needMove){
+            accum += speed * Time.deltaTime;
+            transform.position = Vector3.Lerp(initPos, targetPos, accum);
+            miniMap.transform.position = Vector3.Lerp(initPos/2, targetPos/2, accum);
+
+            if (accum >= 1.0f)
+                needMove = false;
+        }
+	}
 }
---
+++ ProjectOni/Assets/Scripts/Player/ActiveRoom.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActiveRoom : MonoBehaviour
+{
+    private Transform activeRoom = null;
+
+    private void OnTriggerEnter(Collider other) {
+        if(activeRoom != null)
+            ChangeLayer(0);
+        
+        activeRoom = other.gameObject.GetComponent<RoomReference>().thePadre;
+        ChangeLayer(9);
+        Debug.Log(activeRoom.gameObject.layer);
+        Camera.main.GetComponent<CameraFollow>().MoveTo(activeRoom.position);
+    }
+
+    private void ChangeLayer(int layer){
+        foreach (Transform child in activeRoom){
+            if(child.gameObject.layer != layer)
+                child.gameObject.layer = layer;
+        }
+    }
+}
