2c2240aa8375937baaaf1ab0a3f450fa07f69c2d
Alejandro <ale.bros@hotmail.com>
2019-05-18 13:41:39 -0300

    Character Animations implemented

---
+++ ProjectOni/Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class PlayerAnimations : MonoBehaviour
+{
+    Animator anim;
+
+    private void Awake() {
+		anim = GetComponent<Animator>();
+	}
+
+	void Update() 
+	{
+        anim.SetFloat("VerticalSpeed", Input.GetAxis("Vertical"));
+        anim.SetFloat("HorizontalSpeed", Input.GetAxis("Horizontal"));
+	}
+
+    void SetAttackTrigger()
+    {
+        //anim.SetTrigger("Attacking");
+        //anim.SetInteger("Direction", batScript.ActualDirection);
+    }
+
+    public void Death()
+    {
+        anim.SetTrigger("Death");
+
+        enabled = false;
+    }
+}
