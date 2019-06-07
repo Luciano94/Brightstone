aca20a23311e22edefdc3c775143584214f5da82
Alejandro <ale.bros@hotmail.com>
2019-06-07 12:12:26 -0300

    Added Enemy FSM template

---
+++ ProjectOni/Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public abstract class EnemyBase : MonoBehaviour
+{
+    virtual protected void Awake()
+    {
+
+    }
+
+    protected void Update()
+    {
+        OnUpdate();
+    }
+
+    protected void FixedUpdate()
+    {
+        
+    }
+
+    public void EnemyInSight()
+    {
+
+    }
+
+    public void EnemyInAttackRange()
+    {
+        
+    }
+
+    public void StopMoving()
+    {
+
+    }
+
+    abstract protected void OnUpdate();
+
+    virtual protected void OnEnemyInSight()
+    {
+
+    }
+    virtual protected void OnEnemyOutOfSight()
+    {
+
+    }
+    virtual protected void OnEnemyInAttackRange()
+    {
+
+    }
+    virtual protected void OnEnemyOutOfAttackRange()
+    {
+
+    }
+    virtual protected void OnHit()
+    {
+
+    }
+    virtual protected void OnNoHealth()
+    {
+
+    }
+    virtual protected void OnPlayerDeath()
+    {
+
+    }
+
+    public bool PlayerOnSight()
+    {
+        return true;
+    }
+
+    public bool PlayerOnAttackRange()
+    {
+        return true;
+    }
+}
---
+++ ProjectOni/Assets/Scripts/Enemy/EnemyMelee.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class EnemyMelee : Enemy
+{
+    protected override void Patrolling()
+    {
+            
+    }
+
+    protected override void Chasing()
+    {
+
+    }
+
+    protected override void Attacking()
+    {
+
+    }
+
+    protected override void Attack()
+    {
+
+    }
+}
