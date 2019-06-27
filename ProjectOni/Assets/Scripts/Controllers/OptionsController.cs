39b43bf859130ceb2c16c605568ceb1a25e5b21e
Alejandro <ale.bros@hotmail.com>
2019-06-27 15:35:07 -0300

    Added and implemented Controls menu

---
+++ ProjectOni/Assets/Scripts/Controllers/OptionsController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptionsController : MonoBehaviour{
+    [SerializeField] private GameObject controls;
+
+    public void ActivateControls(){
+        controls.SetActive(true);
+    }
+}
