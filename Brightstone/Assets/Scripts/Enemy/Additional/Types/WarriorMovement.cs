48a23fc1124f23420edac71f7b897e52000e9eb2
Alejandro <ale.bros@hotmail.com>
2019-09-20 17:14:00 -0300

    Added Strategy 32 and 33 movement

--- Brightstone/Assets/Scripts/Enemy/Additional/Types/WarriorMovement.cs
+++ Brightstone/Assets/Scripts/Enemy/Additional/Types/WarriorMovement.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WarriorMovement : EnemyMovement{
-    [Header("Strategies vars")]
+    [Header("Strategies Variables")]
     [SerializeField] private float distFromPlayer;
+    [SerializeField] private float rotationSpeedStr33;
+
+    private float currentAngleStr31;
+    private float currentAngleStr33;
+
     override public void ApplyMovementStrategy(int chaserIndex){
         base.ApplyMovementStrategy(chaserIndex);
 
@@ -39,22 +44,47 @@ public class WarriorMovement : EnemyMovement{
 
             // 3 enemies
             case Strategies.Melee31:
+                Vector3 dirPlayerFwd31 = Vector3.forward;
+
+                if (chaserIndex == 2)
+                    dirPlayerFwd31 = Quaternion.AngleAxis(currentAngleStr31 + 225.0f, Vector3.forward) * dirPlayerFwd31;
+                else
+                    dirPlayerFwd31 = Quaternion.AngleAxis(currentAngleStr31 + chaserIndex * 45.0f, Vector3.forward) * dirPlayerFwd31;
+
+                Vector3 objectivePos31 = GameManager.Instance.PlayerPos + dirPlayerFwd31 * distFromPlayer;
+
+                if ((objectivePos31 - transform.position).magnitude > 0.1f)
+                    MoveToObjective(objectivePos31);
+            break;
+            case Strategies.Melee32:
                 Vector3 playerPos = GameManager.Instance.PlayerPos;
                 Vector3 roomOrigin = GameManager.Instance.activeRoom.GetRoomsBehaviour().transform.position;
                 roomOrigin.z = playerPos.z;
-                Vector3 dirPlayerToRoom = (playerPos - roomOrigin).normalized;
+                Vector3 dirPlayerToRoom32 = (playerPos - roomOrigin).normalized;
 
                 if (chaserIndex == 0)
-                    dirPlayerToRoom = Quaternion.AngleAxis(45.0f, Vector3.forward) * dirPlayerToRoom;
+                    dirPlayerToRoom32 = Quaternion.AngleAxis(45.0f, Vector3.forward) * dirPlayerToRoom32;
                 else if (chaserIndex == 2)
-                    dirPlayerToRoom = Quaternion.AngleAxis(-45.0f, Vector3.forward) * dirPlayerToRoom;
+                    dirPlayerToRoom32 = Quaternion.AngleAxis(-45.0f, Vector3.forward) * dirPlayerToRoom32;
 
-                MoveToObjective(playerPos + dirPlayerToRoom * distFromPlayer);
-            break;
-            case Strategies.Melee32:
+                Vector3 objectivePos32 = playerPos + dirPlayerToRoom32 * distFromPlayer;
 
+                if ((objectivePos32 - transform.position).magnitude > 0.1f)
+                    MoveToObjective(objectivePos32);
             break;
             case Strategies.Melee33:
+                currentAngleStr33 += rotationSpeedStr33 * Time.deltaTime;
+                if (currentAngleStr33 >= 360.0f)
+                    currentAngleStr33 -= 360.0f;
+                
+                Vector3 dirPlayerFwd33 = Vector3.forward;
+
+                dirPlayerFwd33 = Quaternion.AngleAxis(currentAngleStr33 + chaserIndex * 120.0f, Vector3.forward) * dirPlayerFwd33;
+
+                Vector3 objectivePos33 = GameManager.Instance.PlayerPos + dirPlayerFwd33 * distFromPlayer;
+
+                if ((objectivePos33 - transform.position).magnitude > 0.1f)
+                    MoveToObjective(objectivePos33);
 
             break;
             case Strategies.Melee34:
@@ -64,5 +94,15 @@ public class WarriorMovement : EnemyMovement{
 
             break;
         }
+    }
+    public void RandomizeAngleStr31()
+    {
+        float newAngle = Random.Range(0.0f, 119.0f);
+        currentAngleStr31 += newAngle * newAngle % 2 == 0 ? 1.0f : -1.0f;
+
+        if (currentAngleStr31 >= 360.0f)
+            currentAngleStr31 -= 360.0f;
+        else if (currentAngleStr31 < 0.0f)
+            currentAngleStr31 += 360.0f;
     }
 }
