5136d71369fff20c5873026af4b02fb1d94987c1
Unknown <lucho.elp40@gmail.com>
2019-10-12 17:31:07 -0300

    Dash implementation

---
+++ Brightstone/Assets/Scripts/Player/PlayerDash.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDash : MonoBehaviour
+{
+    public float dashSpeed;
+    public float startDashTime;
+    private float dashTime;
+
+    private Rigidbody2D playerRB;
+
+    private Vector2 mov;
+    private bool isDashing = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isDashing)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift))
+            {
+                isDashing = true;
+            }
+        }
+        else
+        {
+            if(dashTime <= 0)
+            {
+                isDashing = false;
+                dashTime = startDashTime;
+                playerRB.velocity = Vector2.zero;
+            }
+            else
+            {
+                mov = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+                dashTime -= Time.deltaTime;
+                playerRB.velocity = dashSpeed * mov;
+            }
+        }
+    }
+}
