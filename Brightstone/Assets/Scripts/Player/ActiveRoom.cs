0c315f9e4f6f1c5a5b21f3177007b8b6b4c2ed23
Alejandro <ale.bros@hotmail.com>
2019-12-15 23:19:25 -0300

    Corrected bug the passing through rooms

--- Brightstone/Assets/Scripts/Player/ActiveRoom.cs
+++ Brightstone/Assets/Scripts/Player/ActiveRoom.cs
@@ -11,7 +11,7 @@ public class ActiveRoom : MonoBehaviour
     private NodeExits doorManager;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.layer == 15){
+        if(other.gameObject.layer == 15 && other.name == "Floor"){
             if(activeRoom != null){
                 if(!roomsBehaviour.HaveMarket && !roomsBehaviour.HaveBoss)
                     roomsBehaviour.SetColorNode(normalColor);
